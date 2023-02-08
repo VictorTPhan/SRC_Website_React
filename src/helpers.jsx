5f78d2a39deadd6a604f95e1654f0553be289300
AlexHappyCode <arpenacoding@gmail.com>
2023-02-08 15:31:04 -0800

    implemented case insensitivity

--- src/helpers.jsx
+++ src/helpers.jsx
@@ -1,8 +1,4 @@
 import {v4 as uuid} from 'uuid'
-import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
-import Button from 'react-bootstrap/Button'
 import React from 'react'
 import DocCard from './components/DocCard'
 
@@ -16,15 +12,16 @@ export async function fetchDocuments() {
 
 export function satisfiesKey(item, key) {
   if (!key) return true
+  const keyLower = key.toLowerCase()
   const filename = item.filename.toLowerCase()
   const activityTitle = item['Activity title'].toLowerCase()
   const programmingLanguage = item['Programming language'].toLowerCase()
   const CsTopic = item['CS topic'].toLowerCase()
   return (
-    filename.includes(key.toLowerCase()) ||
-    programmingLanguage.includes(key) ||
-    activityTitle.includes(key) ||
-    CsTopic.includes(key)
+    filename.includes(keyLower) ||
+    programmingLanguage.includes(keyLower) ||
+    activityTitle.includes(keyLower) ||
+    CsTopic.includes(keyLower)
   )
 }
 
