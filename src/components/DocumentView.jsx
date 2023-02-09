a176e66c0ac9cf518bdd37a81b6f2235ec74a238
Alexander Pena <arpenacoding@gmail.com>
2023-02-08 19:20:41 -0800

    adding styling to document view

--- src/components/DocumentView.jsx
+++ src/components/DocumentView.jsx
@@ -7,10 +7,12 @@ import {jsonToUl} from '../helpers2'
 function DocumentView() {
   const document = useLocation().state.element
   return (
-    <Link to="/SRC_Website_React" style={{textDecoration: 'none'}}>
-      <Button>Home</Button>
+    <div id="document-view">
+      <Link to="/SRC_Website_React" style={{textDecoration: 'none'}}>
+        <Button>Home</Button>
+      </Link>
       <ListGroup variant="flush">{jsonToUl(document)}</ListGroup>
-    </Link>
+    </div>
   )
 }
 
