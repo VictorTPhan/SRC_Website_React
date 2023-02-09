54223fb30b20abd9f661cb3790bd28e58d628fd4
Alexander Pena <arpenacoding@gmail.com>
2023-02-08 19:12:27 -0800

    added mdbootstrap

--- src/components/DocCard.jsx
+++ src/components/DocCard.jsx
@@ -2,26 +2,34 @@ import React from 'react'
 import {Card} from 'react-bootstrap'
 import {Link} from 'react-router-dom'
 import PropTypes from 'prop-types'
-import {v4 as uuid} from 'uuid'
 import ListGroup from 'react-bootstrap/ListGroup'
 import {jsonToUl} from '../helpers2'
 
 function DocCard({element, limitAttr}) {
   return (
-    <Link
-      to="/SRC_Website_React/entry"
-      state={{element}}
-      className="Activity-title"
-      style={{textDecoration: 'none'}}>
-      <Card>
-        <Card.Body>
-          <ListGroup variant="flush">
-            <Card.Title>{element['Activity title']}</Card.Title>
-            {jsonToUl(element, limitAttr)}
-          </ListGroup>
-        </Card.Body>
-      </Card>
-    </Link>
+    <div className="bg-image hover-overlay">
+      <Link
+        to="/SRC_Website_React/entry"
+        state={{element}}
+        className="Activity-title"
+        style={{textDecoration: 'none'}}>
+        <Card>
+          <Card.Body>
+            <ListGroup variant="flush">
+              <Card.Title>{element['Activity title']}</Card.Title>
+              {jsonToUl(element, limitAttr)}
+            </ListGroup>
+          </Card.Body>
+        </Card>
+        <div
+          className="mask"
+          style={{
+            background:
+              'linear-gradient(45deg, rgba(29, 236, 197, 0.1), rgba(91, 14, 214, 0.1) 100%)',
+          }}
+        />
+      </Link>
+    </div>
   )
 }
 
